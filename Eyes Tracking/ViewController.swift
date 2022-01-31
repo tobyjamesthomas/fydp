b487d29ed1465dded5b2ef8054f8153ff347b92a
Eddie Ren <eddieren@Eddies-MacBook-Pro.local>
2022-01-30 20:24:55 -0500

    debug key for double blink

--- Eyes Tracking/ViewController.swift
+++ Eyes Tracking/ViewController.swift
@@ -207,6 +207,14 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
                 likeAction()
             case .keyboardLeftArrow:
                 retweetAction()
+            case .keyboardB:
+                // Simulate double (B)link
+                print("Double blink detected!")
+                DispatchQueue.main.async {
+                    self.showMenuViewController()
+                }
+                lastBlinkDate = Date()
+
             default:
                 super.pressesEnded(presses, with: event)
             }
