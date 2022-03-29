aab6ef7a56ae6915a316b17e6cc046e2668bf7b7
Eddie Ren <eddie.ren.2013@gmail.com>
2022-03-29 13:27:06 -0400

    Further reduced to 0.7

--- Eyes Tracking/UserProfileViewController.swift
+++ Eyes Tracking/UserProfileViewController.swift
@@ -516,7 +516,7 @@ class UserProfileViewController: UIViewController, ARSCNViewDelegate, ARSessionD
             if eyeBlinkLeft < 0.4 && eyeBlinkRight < 0.4 {
                 if isBlinking == true {
                     let elapsed = Date().timeIntervalSince(lastBlinkDate)
-                    if elapsed < 1.0 {
+                    if elapsed < 0.7 {
                         print("Double blink detected!")
                         DispatchQueue.main.async {
                             self.showUserMenuViewController()
