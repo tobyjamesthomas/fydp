eade071a76d198af3d7bd11f9c482bd6eb1cff77
Kieryn Davison <kmdaviso@uwaterloo.ca>
2022-04-04 00:21:32 -0400

    fix build errors

--- Package.swift
+++ Package.swift
@@ -13,8 +13,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Eyes Tracking",
-            path: ""),
+            name: "Eyes Tracking"),
         .testTarget(
             name: "fydpTests",
             dependencies: ["fydp"])
